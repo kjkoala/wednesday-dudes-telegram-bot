2957ec44892fa1d390d5a4fa31b216d9ea181e2a
Michael Radionov <micrddrgn@gmail.com>
2018-12-13 00:11:46 +0300

    Add checks

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,38 @@
+const METHOD_POST = 'POST';
+
 exports.handler = (event, context, callback) => {
-  console.log('hello called', event.httpMethod, event.path, event.queryStringParameters);
-  console.log('body', event.body);
+  const httpMethod = event.httpMethod;
+
+  if (httpMethod !== METHOD_POST) {
+    callback(null, {
+      statusCode: 405,
+      body: 'Method not allowed',
+    });
+    console.error('Method not allowed', httpMethod);
+    return;
+  }
+
+  const body = event.body;
+
+  let data;
+
+  try {
+    data = JSON.parse(body);
+  } catch (err) {
+    console.error('Failed to parse body as JSON', body);
+  }
+
+  if (!data) {
+    callback(null, {
+      statusCode: 400,
+      body: 'Bad request',
+    });
+    console.error('Bad request', httpMethod);
+    return;
+  }
+
+  console.log('body', data);
+
   callback(null, {
     statusCode: 200,
     body: 'Hello world',
