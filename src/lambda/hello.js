6149532e073768a940f6cd4cf51dae3e6571d3fe
Michael Radionov <micrddrgn@gmail.com>
2018-12-12 23:59:53 +0300

    Add logging

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,4 +1,5 @@
 exports.handler = (event, context, callback) => {
+  console.log('hello called', event.path, event.queryStringParameters);
   callback(null, {
     statusCode: 200,
     body: 'Hello world',
