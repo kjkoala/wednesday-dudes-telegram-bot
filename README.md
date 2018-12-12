d4824e90597b69e958977835f1dcd9884b6a2758
Michael Radionov <micrddrgn@gmail.com>
2018-12-13 00:05:21 +0300

    Add logging for body

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 wednesday-dudes-telegram-bot
 ===
+
+Set telegram bot webhook
+
+```bash
+curl \
+  -F "url=https://<subdomain>.netlify.com/.netlify/functions/<function>" \
+  https://api.telegram.org/bot<token>/setWebhook
+```
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,5 +1,6 @@
 exports.handler = (event, context, callback) => {
-  console.log('hello called', event.path, event.queryStringParameters);
+  console.log('hello called', event.httpMethod, event.path, event.queryStringParameters);
+  console.log('body', event.body);
   callback(null, {
     statusCode: 200,
     body: 'Hello world',
